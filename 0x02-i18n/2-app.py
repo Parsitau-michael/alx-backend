b47e05861ceac701497608ab009ea99472ff0423
Parsitau-michael <parsitaumichael@gmail.com>
2024-11-06 13:04:02 +0000

    Corrected errors on my files

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+This module sets up a basic Flask app
+"""
+
+
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    A class to configure available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config) # Load configuration from the Config class
+
+babel =Babel(app) # Initialize Babel with the Flask app
+
+
+@babel.localeselector
+def get_locale():
+    """
+    A function to determine the users best locale match with our
+    supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """
+    A function defining the route
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
