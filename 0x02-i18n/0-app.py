78b0c06116d04c315aa7b606ec6a9f1aa4a11531
Parsitau-michael <parsitaumichael@gmail.com>
2024-11-06 11:08:09 +0000

    Installed and Setup Babel

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,9 +5,22 @@ This module sets up a basic Flask app
 
 
 from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    A class to configure available languages in our app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
+app.config.from_object(Class) # Load configuration from the Config class
+
+babel =Babel(app) # Initialize Babel with the Flask app
 
 
 @app.route('/')
